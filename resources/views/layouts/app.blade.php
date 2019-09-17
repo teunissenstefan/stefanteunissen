007d29182908966bbcdbb294279d66abb0d47c97
Stefan Teunissen <stefanteunissen1@gmail.com>
2019-09-17 13:35:36 +0200

    Website icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    @include("includes.favicon")
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
