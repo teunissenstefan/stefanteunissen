7daf9c939ad9a306442e33b2c2d87e638734f4a2
Stefan Teunissen <stefanteunissen1@gmail.com>
2021-04-27 22:23:00 +0200

    Migration for hidden

--- resources/views/includes/element.blade.php
+++ resources/views/includes/element.blade.php
@@ -5,27 +5,29 @@
         @case("App\\CardList")
             <div class="row" data-masonry='{"percentPosition": true }'>
                 @foreach($element->elementable->cardItems as $cardItem)
-                    <div class="col-12 col-md-4 mb-3 ml-auto mr-auto">
-                        <div class="card" style="border:1px solid rgba(0, 0, 0, .5);">
-                            <a href="{{asset("storage/images/".$cardItem->image)}}" data-toggle="lightbox">
-                                <img class="card-img-top" src="{{asset("storage/images/".$cardItem->image)}}" alt="Card image cap">
-                            </a>
-                            <div class="card-body">
-                                <h5 class="card-title">{{$cardItem->title}}</h5>
-                                <p class="card-text">{!! $cardItem->description !!}</p>
-                                @if($cardItem->btn_code != null) <a href="{{$cardItem->btn_code}}" target="_blank" class="btn btn-primary btn-orange"><i class="fa fa-code"></i> Code</a> @endif
-                                @if($cardItem->btn_demo != null) <a href="{{$cardItem->btn_demo}}" target="_blank" class="btn btn-primary btn-orange"><i class="fa fa-eye"></i> Demo</a> @endif
-                                @php($technologies = json_decode($cardItem->technologies))
-                                @if(count((array)$technologies) > 1 || (count((array)$technologies) > 0 && $technologies[0]!=null))
-                                    <hr/>
-                                    <h6>Technologiën</h6>
-                                    @foreach($technologies as $technology)
-                                        <span class="badge badge-secondary">{{$technology}}</span>
-                                    @endforeach
-                                @endif
+                    @if($cardItem->hidden==0)
+                        <div class="col-12 col-md-4 mb-3 ml-auto mr-auto">
+                            <div class="card" style="border:1px solid rgba(0, 0, 0, .5);">
+                                <a href="{{asset("storage/images/".$cardItem->image)}}" data-toggle="lightbox">
+                                    <img class="card-img-top" src="{{asset("storage/images/".$cardItem->image)}}" alt="Card image cap">
+                                </a>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$cardItem->title}}</h5>
+                                    <p class="card-text">{!! $cardItem->description !!}</p>
+                                    @if($cardItem->btn_code != null) <a href="{{$cardItem->btn_code}}" target="_blank" class="btn btn-primary btn-orange"><i class="fa fa-code"></i> Code</a> @endif
+                                    @if($cardItem->btn_demo != null) <a href="{{$cardItem->btn_demo}}" target="_blank" class="btn btn-primary btn-orange"><i class="fa fa-eye"></i> Demo</a> @endif
+                                    @php($technologies = json_decode($cardItem->technologies))
+                                    @if(count((array)$technologies) > 1 || (count((array)$technologies) > 0 && $technologies[0]!=null))
+                                        <hr/>
+                                        <h6>Technologiën</h6>
+                                        @foreach($technologies as $technology)
+                                            <span class="badge badge-secondary">{{$technology}}</span>
+                                        @endforeach
+                                    @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
         @break
